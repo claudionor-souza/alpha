85c9a4750eb3f788adc58c0f434a1d0916a908a3
claudionor-souza <claudiomeirasp@gmail.com>
2022-10-28 00:58:32 -0300

    Create conversor_medidas.py
    Converte cm em mm, metros em centímetro etc

---
+++ conversor_medidas.py
@@ -0,0 +1,11 @@
+print('CONVERSOR DE MEDIDAS')
+print('Deseja converter qual medida? ')
+print('''[1]CM para MM
+[2]MM para CM
+[3]CM para M
+[4]CM para POLEGADAS
+[5]POLEGADAS para CM''')
+if opcao == 1:
+    medida = float(input('Medida: '))
+    resultado = medida * 10
+
